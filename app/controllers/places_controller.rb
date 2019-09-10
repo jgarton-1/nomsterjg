b29221fc4504600c8e3c1f0e5b20c022181f0a03
Jonathan Garton <jon.garton94@gmail.com>
2019-09-10 23:13:40 +0000

    image upload error with photo_controller.rb

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -22,6 +22,7 @@ class PlacesController < ApplicationController
   def show
     @place = Place.find(params[:id])
     @comment = Comment.new
+    @photo = Photo.new
     @GOOGLE_API_KEY = ENV['GOOGLE_API_KEY']
   end
 
