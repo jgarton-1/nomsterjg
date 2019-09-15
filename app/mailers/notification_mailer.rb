59a06614d855eba979f94a3f718ebdc9edbe3220
Jonathan Garton <jon.garton94@gmail.com>
2019-09-15 18:51:49 +0000

    preview the email failure, no email sent

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,7 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
   def comment_added
-    mail(to: "jon.garton@gmail.com",
+    mail(to: "altjon.garton@gmail.com",
           subject: "a comment has been added to your place")
   end
 end
